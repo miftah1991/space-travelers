d29c29fb23987c4ee3badf148ee0871051d8ae79
miftah1991 <miftah.amin1991@gmail.com>
2022-04-11 15:23:11 +0430

    Add Mission and Rocker pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import { Route, Routes } from 'react-router-dom';
 import MainHeader from './components/MainHeader';
+import MissionsPage from './pages/MissionsPage';
+import RocketsPage from './pages/RocketsPage';
 
 function App() {
   return (
-    <div className="App">
-       <MainHeader />
-    </div>
+    <>
+      <MainHeader />
+      <Routes>
+        <Route path="/" element={<RocketsPage />} />
+        <Route path="/missions" element={<MissionsPage />} />
+      </Routes>
+    </>
   );
 }
 
