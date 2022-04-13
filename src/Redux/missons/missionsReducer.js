f1fadeca4b516b12975e780d6b1a32e2b4701804
ohamed Eldeeb <agnabiieldeeeb@gmail.com>
2022-04-13 22:47:05 +0200

    Fix linter errors

--- src/Redux/missons/missionsReducer.js
+++ src/Redux/missons/missionsReducer.js
@@ -1,27 +1,25 @@
-import getMissions from "./missionsAPIHelper";
+import getMissions from './missionsAPIHelper';
 
-const GET_ALL_MISSIONS = "GET_ALL_MISSIONS";
-const TOGGLE_MISSION = "TOGGLE_MISSION";
+const GET_ALL_MISSIONS = 'GET_ALL_MISSIONS';
+const TOGGLE_MISSION = 'TOGGLE_MISSION';
 
 const initialState = [
   {
-    mission_id: "",
-    mission_name: "",
-    description: "",
+    mission_id: '',
+    mission_name: '',
+    description: '',
     reserved: false,
   },
 ];
 
 export const getAllMissions = () => async (dispatch) => {
   const missions = await getMissions();
-  const missionsForDispatch = missions.map((mission) => {
-    return {
-      mission_id: mission.mission_id,
-      mission_name: mission.mission_name,
-      description: mission.description,
-      reserved: false,
-    };
-  });
+  const missionsForDispatch = missions.map((mission) => ({
+    mission_id: mission.mission_id,
+    mission_name: mission.mission_name,
+    description: mission.description,
+    reserved: false,
+  }));
   dispatch({
     type: GET_ALL_MISSIONS,
     payload: missionsForDispatch,
