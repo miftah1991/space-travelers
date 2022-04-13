8b290afe780fed4afa92a9475cff89f706722f69
CRClothier <c.r.clothier@gmail.com>
2022-04-12 23:29:58 -0500

    Update myprofile page

--- src/pages/MyProfile.jsx
+++ src/pages/MyProfile.jsx
@@ -5,10 +5,17 @@ import './MyProfile.css';
 function MyProfile() {
   const rockets = useSelector((state) => state.rockets);
   const [reservedRockets, setReservedRockets] = useState([]);
+  const dragons = useSelector((state) => state.dragons);
+  const [reservedDragons, setReservedDragons] = useState([]);
 
   useEffect(() => {
     const resRockets = rockets.filter((rocket) => rocket.reserved && rocket);
     setReservedRockets(resRockets);
+  }, []);
+
+  useEffect(() => {
+    const resDragons = dragons.filter((dragon) => dragon.reserved && dragon);
+    setReservedDragons(resDragons);
   }, []);
 
   return (
@@ -17,6 +24,12 @@ function MyProfile() {
         <h1>My Rockets</h1>
         <ul className="list">
           {reservedRockets.map((rocket) => <li key={rocket.id}>{rocket.name}</li>)}
+        </ul>
+      </div>
+      <div>
+        <h1>My Dragons</h1>
+        <ul className="list">
+          {reservedDragons.map((dragon) => <li key={dragon.id}>{dragon.name}</li>)}
         </ul>
       </div>
     </div>
